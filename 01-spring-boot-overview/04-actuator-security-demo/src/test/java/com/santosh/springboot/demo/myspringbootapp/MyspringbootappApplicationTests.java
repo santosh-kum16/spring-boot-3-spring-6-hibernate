c8ef7811dfd85c3855994b79217bba0395908adc
Santosh Kumar <santosh.kum16@gmail.com>
2025-06-21 14:51:46 +0530

    sprint boot security added

---
+++ 01-spring-boot-overview/04-actuator-security-demo/src/test/java/com/santosh/springboot/demo/myspringbootapp/MyspringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.springboot.demo.myspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
