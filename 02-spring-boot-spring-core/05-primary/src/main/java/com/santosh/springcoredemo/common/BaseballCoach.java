116f74b4560b953c067e426fdd0cd54e877a7258
Santosh Kumar <santosh.kum16@gmail.com>
2025-06-22 11:49:05 +0530

    only one @Primary shuld use

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/santosh/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.santosh.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes baseball batting practice";
+    }
+}
---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/santosh/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,12 @@
+package com.santosh.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5000!!!";
+    }
+}
