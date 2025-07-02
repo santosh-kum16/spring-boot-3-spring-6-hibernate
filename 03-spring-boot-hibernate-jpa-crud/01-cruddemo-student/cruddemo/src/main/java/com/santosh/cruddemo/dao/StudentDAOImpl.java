4f774c749f64fe343c610ce83861b88869213b79
Santosh Kumar <santosh.kum16@gmail.com>
2025-07-02 19:23:33 +0530

    find all method Implementation

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/santosh/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/santosh/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.santosh.cruddemo.dao;
 
 import com.santosh.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO{
@@ -29,5 +32,14 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        // return query results
+        return theQuery.getResultList();
     }
 }
